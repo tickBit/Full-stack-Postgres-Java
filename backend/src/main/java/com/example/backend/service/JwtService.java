8852d9c11f305fdda6074b2a02e3939ca7415ad6
tickBit <marjukosk@gmail.com>
2025-08-19 14:59:25 +0300

    Add app config

--- backend/src/main/java/com/example/backend/service/JwtService.java
+++ backend/src/main/java/com/example/backend/service/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
+
     @Value("${security.jwt.secret-key}")
     private String secretKey;
 
